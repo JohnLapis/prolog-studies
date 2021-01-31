c90124cd4a93efc91bfd9d0f360309d51df62282
john <borges-2014@live.com>
2021-01-31 19:37:50 -0300

    refactor: crossword.pl

--- learn_prolog_now/crossword.pl
+++ learn_prolog_now/crossword.pl
@@ -29,12 +29,20 @@ word(cobalto,  c,o,b,a,l,t,o).
 word(pistola,  p,i,s,t,o,l,a).
 word(statale,  s,t,a,t,a,l,e).
 
-crossword(word(V1_word, V1_1, V1_2, V1_3, V1_4, V1_5, V1_6, V1_7),
-          word(V2_word, V2_1, V2_2, V2_3, V2_4, V2_5, V2_6, V2_7),
-          word(V3_word, V3_1, V3_2, V3_3, V3_4, V3_5, V3_6, V3_7),
-          word(H1_word, H1_1, H1_2, H1_3, H1_4, H1_5, H1_6, H1_7),
-          word(H2_word, H2_1, H2_2, H2_3, H2_4, H2_5, H2_6, H2_7),
-          word(H3_word, H3_1, H3_2, H3_3, H3_4, H3_5, H3_6, H3_7)):-
+
+unique([]).
+unique([X|Xs]):- not(member(X,Xs)), unique(Xs).
+
+crossword(V1_word, V2_word, V3_word, H1_word, H2_word, H3_word):-
+    word(V1_word, _, V1_2, _, V1_4, _, V1_6, _),
+    word(V2_word, _, V2_2, _, V2_4, _, V2_6, _),
+    word(V3_word, _, V3_2, _, V3_4, _, V3_6, _),
+    word(H1_word, _, H1_2, _, H1_4, _, H1_6, _),
+    word(H2_word, _, H2_2, _, H2_4, _, H2_6, _),
+    word(H3_word, _, H3_2, _, H3_4, _, H3_6, _),
+
+    unique([V1_word, V2_word, V3_word, H1_word, H2_word, H3_word]),
+
     V1_2 = H1_2,
     V1_4 = H2_2,
     V1_6 = H3_2,
